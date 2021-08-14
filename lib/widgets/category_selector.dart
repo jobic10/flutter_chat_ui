8ed46c006e2a712d8977176fb2b7c29567b4a1b7
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-15 00:24:11 +0100

    Change category

--- lib/widgets/category_selector.dart
+++ lib/widgets/category_selector.dart
@@ -18,15 +18,22 @@ class _CategorySelectorState extends State<CategorySelector> {
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-            child: Text(
-              categories[index],
-              style: TextStyle(
-                color: index == selectedIndex ? Colors.white : Colors.white60,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.2,
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              child: Text(
+                categories[index],
+                style: TextStyle(
+                  color: index == selectedIndex ? Colors.white : Colors.white60,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.2,
+                ),
               ),
             ),
           );
