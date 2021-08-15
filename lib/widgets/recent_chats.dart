8effb779b964cdfb63506c35a0559c2f94719888
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-15 15:40:17 +0100

    Chat saved

--- lib/widgets/recent_chats.dart
+++ lib/widgets/recent_chats.dart
@@ -18,58 +18,68 @@ class RecentChats extends StatelessWidget {
             topRight: Radius.circular(30),
           ),
         ),
-        child: ListView.builder(
-          itemBuilder: (context, index) {
-            final Message chat = chats[index];
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              final Message chat = chats[index];
+              return Container(
+                margin: EdgeInsets.only(top: 5, bottom: 5, right: 20),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    CircleAvatar(
-                      radius: 35,
-                      backgroundImage: AssetImage(chat.sender.imageUrl),
-                    ),
-                    SizedBox(width: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Row(
                       children: [
-                        Text(
-                          chat.sender.name,
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          overflow: TextOverflow.ellipsis,
+                        CircleAvatar(
+                          radius: 35,
+                          backgroundImage: AssetImage(chat.sender.imageUrl),
                         ),
-                        SizedBox(height: 5),
-                        Container(
-                          width: MediaQuery.of(context).size.width * 0.5,
-                          child: Text(
-                            chat.text,
-                            style: TextStyle(
-                              color: Colors.blueGrey,
-                              fontSize: 15,
-                              fontWeight: FontWeight.w600,
+                        SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              chat.sender.name,
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              overflow: TextOverflow.ellipsis,
                             ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
+                            SizedBox(height: 5),
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.5,
+                              child: Text(
+                                chat.text,
+                                style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
                         ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(chat.time),
+                        Text('NEW'),
                       ],
                     ),
                   ],
                 ),
-                Column(
-                  children: [
-                    Text(chat.time),
-                    Text('NEW'),
-                  ],
-                ),
-              ],
-            );
-          },
-          itemCount: chats.length,
+              );
+            },
+            itemCount: chats.length,
+          ),
         ),
       ),
     );
