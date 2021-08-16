a0d5e41089c718e36f52f672e4f8b3d75cc94ed4
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-16 13:24:46 +0100

    Chat Screen

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_ui/data/faker.dart';
+import 'package:chat_ui/models/message.dart';
 import 'package:chat_ui/models/user.dart';
 import 'package:flutter/material.dart';
 
@@ -34,6 +36,61 @@ class _ChatScreenState extends State<ChatScreen> {
             onPressed: () {},
           )
         ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+                child: ListView.builder(
+                  padding: EdgeInsets.only(top: 15),
+                  itemBuilder: (context, index) {
+                    final message = messages[index];
+                    final isMe = message.sender.id == currentUser.id;
+                    return _buildMessage(message, isMe);
+                  },
+                  itemCount: messages.length,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMessage(Message message, bool isMe) {
+    return Container(
+      margin: isMe
+          ? EdgeInsets.only(top: 8, bottom: 8, left: 80)
+          : EdgeInsets.only(top: 8, bottom: 8, right: 80),
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+      child: Text(
+        message.text,
+        style: TextStyle(),
+      ),
+      decoration: BoxDecoration(
+        color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+        borderRadius: isMe
+            ? BorderRadius.only(
+                topLeft: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+              )
+            : BorderRadius.only(
+                topRight: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
       ),
     );
   }
